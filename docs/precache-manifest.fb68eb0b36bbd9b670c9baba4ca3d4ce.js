0b9b3e109c347b04e4f2865ad2708b351b55a7e9
Mauricio Cartagena <mc973220@gmail.com>
2020-10-11 13:14:35 -0400

    creando deploy

---
+++ docs/precache-manifest.fb68eb0b36bbd9b670c9baba4ca3d4ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2da3414bcc1252e41a41b4c3d11aaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98740f28fc49063c9c72",
+    "url": "/static/css/main.0398049a.chunk.css"
+  },
+  {
+    "revision": "fab77e7666bdc29faeaf",
+    "url": "/static/js/2.60e51318.chunk.js"
+  },
+  {
+    "revision": "94efaf9ef8dfdcc9b58d075c8086b235",
+    "url": "/static/js/2.60e51318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98740f28fc49063c9c72",
+    "url": "/static/js/main.37c00a67.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
