f9cedaa2c96095c0c96b04005e35fadab1232d94
Mauricio Cartagena <mc973220@gmail.com>
2020-10-14 19:32:12 -0400

    se agrego el env para manejar información delicada

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,16 +3,36 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrBiLD7cpyRuOR6RpmAA0ubNQFXNACnoI",
-    authDomain: "react-app-cursos-2ff71.firebaseapp.com",
-    databaseURL: "https://react-app-cursos-2ff71.firebaseio.com",
-    projectId: "react-app-cursos-2ff71",
-    storageBucket: "react-app-cursos-2ff71.appspot.com",
-    messagingSenderId: "96306682559",
-    appId: "1:96306682559:web:53bd28eb4f62b17044840a"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
 };
 
-firebase.initializeApp(firebaseConfig);
+// const firebaseConfigTesting = {
+//     apiKey: "AIzaSyBqitUaYKn_2Jk5Fu4B49swbPtG8oDbEDs",
+//     authDomain: "redux-demo-34e4a.firebaseapp.com",
+//     databaseURL: "https://redux-demo-34e4a.firebaseio.com",
+//     projectId: "redux-demo-34e4a",
+//     storageBucket: "redux-demo-34e4a.appspot.com",
+//     messagingSenderId: "82677216063",
+//     appId: "1:82677216063:web:43e7f7197de23db5c0d98a"
+// };
+
+// if( process.env.NODE_ENV === 'test' ){
+//     //testing
+//     firebase.initializeApp(firebaseConfigTesting);
+
+// }else{
+//     //dev/prod
+//     firebase.initializeApp(firebaseConfig);
+// }
+
+    firebase.initializeApp(firebaseConfig);
+
 
 const db = firebase.firestore();
 
